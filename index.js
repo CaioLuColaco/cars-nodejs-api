96a615aa0bee1627e2d0c144c25549ddd382637c
caio lucena <caio.lcolaco@gmail.com>
2022-11-02 20:28:58 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const routes = require("./src/routes")
+
+const app = express()
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log(`SERVER IS RUNNING ON PORT: ${this.address().port}!`)
+})
