c7165236d48f57bfcfb5d930164226320b4aab33
caio lucena <caio.lcolaco@gmail.com>
2022-11-08 18:35:57 -0300

    routes created

--- src/routes.js
+++ src/routes.js
@@ -2,3 +2,13 @@ const express = require('express')
 
 const routes = express.Router()
 
+const CarsController = require('./controllers/CarsControllers')
+
+// Imoveis
+routes.get('/car',           CarsController.findAll)
+routes.get('/car/:id',       CarsController.find)
+routes.post('/car',          CarsController.create)
+routes.put('/car/:id',       CarsController.update)
+routes.delete('/car/:id',    CarsController.delete)
+
+module.exports = routes;
