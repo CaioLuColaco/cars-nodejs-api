4631d8c084ee7232ec6cac404cde92264a5ddfbb
caio lucena <caio.lcolaco@gmail.com>
2022-11-08 20:20:56 -0300

    added documentation on adress/doc

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const routes = require("./src/routes")
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
 
 const app = express()
 
 app.use(express.json())
-app.use(routes)
+app.use(routes, swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.listen(process.env.PORT || 3000, function(){
     console.log(`SERVER IS RUNNING ON PORT: ${this.address().port}!`)
