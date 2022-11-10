bf5279f22f9926aeca2e113e29e7801737700ee8
caio lucena <caio.lcolaco@gmail.com>
2022-11-10 15:24:05 -0300

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const routes = require("./src/routes")
+const cors = require('cors')
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger_output.json')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes, swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
